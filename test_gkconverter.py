cbc56a113d4ba5938d000f527af83f215e414d9a
Niko W <der@nik0.de>
2012-06-10 00:08:05 +0200

    More intuitive name.

---
+++ test_gkconverter.py
@@ -0,0 +1,29 @@
+import unittest
+import gkconverter as GKConverter
+
+class GKConverterTest(unittest.TestCase):
+	def testSimpleConversion(self):
+		(x, y) = GKConverter.convert_GK_to_lat_long(3477733, 5553274)
+
+		self.assertAlmostEqual(50.11526435691097, x)
+		self.assertAlmostEqual(8.687625204011725, y)
+
+	def testSimpleGKtoLatLongConversion(self):
+		(x, y) = GKConverter.convertGaussKruegerToLatitudeLongitude(3477733, 5553274)
+
+		self.assertAlmostEqual(50.1164273930041, x)
+		self.assertAlmostEqual(8.6886330000005, y)
+
+	def testHelmertTransformation(self):
+		(x, y) = GKConverter.sevenParameterHelmertTransformation(50.1164273930041, 8.6886330000005)
+
+		self.assertAlmostEqual(50.11526435691097, x)
+		self.assertAlmostEqual(8.687625204011725, y)
+
+	def testWrongInput(self):
+		for (right, height) in ((100000, 1000000), (1000000, 100000), (1000000, 1000000)):
+			self.assertRaises(ValueError, GKConverter.convert_GK_to_lat_long, right, height)
+			self.assertRaises(ValueError, GKConverter.convert_GK_to_lat_long, right, height)
+
+if __name__ == '__main__':
+	unittest.main()
